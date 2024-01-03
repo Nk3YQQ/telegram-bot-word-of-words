a6ca9528ed5a861603eb33b7d88b640885c38f6f
Egor Nikitin <eganik5674@gmail.com>
2024-01-03 22:35:33 +0300

    Update classes/class_basic_word.py

--- classes/class_basic_word.py
+++ classes/class_basic_word.py
@@ -3,24 +3,25 @@ class BasicWord:
     Класс, содержащий поля: исходное слово
     и набор допустимых слов
     """
-    def __init__(self, word, subwords):
+
+    def __init__(self, word: str, subwords: list) -> None:
         self.word = word
         self.subwords = subwords
 
-    def check_word(self, user_input):
+    def check_word(self, user_input: str) -> bool:
         """
         Возвращает True, если слово есть в списке подслов
         или False, если слова нет в списке
         """
         return user_input in self.subwords
 
-    def count_subwords(self):
+    def count_subwords(self) -> int:
         """
         Возвращает количество подслов
         в списке
         """
         return len(self.subwords)
 
-    def __str__(self):
+    def __str__(self) -> str:
         return f"""Составьте {self.count_subwords()} слов из слова {self.word.upper()}
 Слова должны быть не короче {len(min(self.subwords, key=len))} букв"""
