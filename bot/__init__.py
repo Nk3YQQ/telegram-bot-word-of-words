7ad1d42878b8ee3875a543d20e5d397e71db17be
Egor Nikitin <eganik5674@gmail.com>
2024-01-03 22:35:01 +0300

    Update bot/

--- bot/__init__.py
+++ bot/__init__.py
@@ -1,14 +1,13 @@
 import os
 
-from classes.class_word_of_words import WordOfWords
-
 from dotenv import load_dotenv
 
 from classes.class_bot import Bot
+from classes.class_word_of_words import WordOfWords
 
 load_dotenv()
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     bot = Bot(os.getenv("TELEBOT_API"))
 
     game = WordOfWords(bot)
