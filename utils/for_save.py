9c0c74f4e853db74ac7d49864e61c4b295272ff7
Egor Nikitin <eganik5674@gmail.com>
2024-01-03 22:36:15 +0300

    Update utils/for_save.py

--- utils/for_save.py
+++ utils/for_save.py
@@ -1,29 +1,31 @@
 import json
+from pathlib import Path
+from typing import Any
 
 
-def open_json_file(filepath):
+def open_json_file(filepath: Path) -> list[dict] | Any:
     with open(filepath, "r", encoding="utf-8") as file:
         return json.load(file)
 
 
-def write_to_json_file(filepath, data):
+def write_to_json_file(filepath: Path, data: Any) -> Any:
     with open(filepath, "w", encoding="utf-8") as file:
         return json.dump(data, file, ensure_ascii=False, indent=4)
 
 
-def write_score_to_player(players_data, player_name, score):
+def write_score_to_player(players_data: Any, player_name: str, score: int) -> Any:
     player_exists = False
     for player_data in players_data:
-        if player_data.get('name') == player_name:
-            player_data['score'].append(score)
+        if player_data.get("name") == player_name:
+            player_data["score"].append(score)
             player_exists = True
     if not player_exists:
-        new_player = {'name': player_name, 'score': [score]}
+        new_player = {"name": player_name, "score": [score]}
         players_data.append(new_player)
     return players_data
 
 
-def get_top_five_scores(players_data, player_name):
+def get_top_five_scores(players_data: Any, player_name: str) -> Any:
     for player_data in players_data:
-        if player_data.get('name') == player_name:
-            return max(player_data['score'])
+        if player_data.get("name") == player_name:
+            return max(player_data["score"])
