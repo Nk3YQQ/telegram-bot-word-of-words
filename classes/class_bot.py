06b74745d36f778f56f842bc2a84bb8653252e13
Egor Nikitin <eganik5674@gmail.com>
2024-01-04 12:17:47 +0300

    Update classes/class_bot.py

--- classes/class_bot.py
+++ classes/class_bot.py
@@ -4,5 +4,9 @@ import telebot
 
 
 class Bot(telebot.TeleBot):
+    """
+    Класс является абстракцией для создания телеграм-бота
+    """
+
     def __init__(self, api_key: str | None, *args: Any, **kwargs: Any) -> None:
         super().__init__(api_key, *args, **kwargs)
