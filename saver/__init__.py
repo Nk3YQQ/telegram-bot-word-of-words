5ceb07707aa30c91f55fa78946d2008a687fb046
Egor Nikitin <eganik5674@gmail.com>
2024-01-04 12:17:21 +0300

    Update saver/

--- saver/__init__.py
+++ saver/__init__.py
@@ -2,25 +2,45 @@ from abc import ABC, abstractmethod
 from pathlib import Path
 from typing import Any
 
-from utils.for_save import get_top_five_scores, open_json_file, write_score_to_player, write_to_json_file
+from utils.for_save import get_top_score, open_json_file, write_score_to_player, write_to_json_file
 
 
 class Saver(ABC):
+    """
+    Класс является абстракцией для сохранения результата пользователя
+    """
+
     @abstractmethod
     def save_result(self, player_name: str, score: int, filepath: Path) -> None:
+        """
+        Абстрактный метод реализует сохранение результатов пользователя
+        """
         pass
 
     @abstractmethod
-    def get_top_five_results(self, player_name: str, filepath: Path) -> None:
+    def get_top_result(self, player_name: str, filepath: Path) -> None:
+        """
+        Абстрактный метод выводит лучшей результат пользователя
+        """
         pass
 
 
 class JSONSaver(Saver):
+    """
+    Класс работает с сохранением результата пользователя в json-файл
+    """
+
     def save_result(self, player_name: str, score: int, filepath: Path) -> None:
+        """
+        Метод реализует сохранение результатов пользователя в json-файл
+        """
         players_data = open_json_file(filepath)
         upgraded_players_data = write_score_to_player(players_data, player_name, score)
         write_to_json_file(filepath, upgraded_players_data)
 
-    def get_top_five_results(self, player_name: str, filepath: Path) -> Any:
+    def get_top_result(self, player_name: str, filepath: Path) -> Any:
+        """
+        Метод выводит лучшей результат пользователя из json-файла
+        """
         players_data = open_json_file(filepath)
-        return f"Максимальное количество угаданных слов: {get_top_five_scores(players_data, player_name)}"
+        return f"Максимальное количество угаданных слов: {get_top_score(players_data, player_name)}"
